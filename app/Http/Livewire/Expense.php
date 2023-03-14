a1dd468b52663e68a370472d03e1b3c874e79682
Egbeleke Daniel <www.daniko15@gmail.com>
2023-03-14 18:18:20 +0100

    ui buildup

--- app/Http/Livewire/Expense.php
+++ app/Http/Livewire/Expense.php
@@ -4,12 +4,26 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Category;
+use Livewire\WithFileUploads;
 
 class Expense extends Component
 {
+    use WithFileUploads;
+ 
+
+    public $photo;
+ 
+    public function updatedPhoto()
+    {
+        $this->validate([
+            'photo' => 'image|max:1024',
+        ]);
+    }
+
+
     public function render()
     {
         $cat = Category::all();
-        return view('livewire.expense');
+        return view('livewire.expense', compact ('cat'));
     }
 }
