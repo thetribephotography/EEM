b5eee491ff3ccabf90d467a0d40afd5dee7fe225
Egbeleke Daniel <www.daniko15@gmail.com>
2023-03-14 15:17:59 +0100

    livewire ui implementations 20%

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\Expense;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +27,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/expense', [Expense::class, 'render'])->name('expense.form');
 });
 
 require __DIR__.'/auth.php';
